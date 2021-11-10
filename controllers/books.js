8c140cd70b2da0b156cd123298bf6940ba823ddf
WangManLex <alexandermaniwang99@gmail.com>
2021-11-10 11:04:12 -0500

    fix control flow

--- controllers/books.js
+++ controllers/books.js
@@ -2,6 +2,7 @@ import { Book } from "../models/book.js";
 
 function index(req, res){
   Book.find({}, function(err, books) {
+    console.log(books)
     res.render('books/index', {
       books
     })
